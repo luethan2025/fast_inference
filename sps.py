63c90463d99e188f94a076aa5aa6b252c3177410
Ethan Lu <luethan2025@outlook.com>
2024-03-18 02:14:49 -0400

    fixed formatting

--- sps.py
+++ sps.py
@@ -6,9 +6,17 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 def get_tokenizer(name):
   if name == "small":
-    return T5Tokenizer.from_pretrained("google-t5/t5-small")
+    return T5Tokenizer.from_pretrained(
+                        "google-t5/t5-small",
+                         model_max_length=512,
+                         truncation=True,
+                         return_tensors="pt")
   elif name == "base":
-    return T5Tokenizer.from_pretrained("google-t5/t5-base")
+    return T5Tokenizer.from_pretrained(
+                        "google-t5/t5-base",
+                        model_max_length=512,
+                        truncation=True,
+                        return_tensors="pt")
   else:
     raise NotImplementedError
 
@@ -26,16 +34,24 @@ def main(target, draft, prompt, target_sequence_length):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(prog="sps.py",
-                                   description='speculative sampling')
-  parser.add_argument('--target', required=True,
-                                  help="auto-regressive target model")
-  parser.add_argument('--draft', required=True,
-                                 help="auto-regressive draft model")
-  parser.add_argument('--prompt', required=True,
-                                  help="initial prompt sequence")
-  parser.add_argument('--target_sequence_length', required=True,
-                                                  type=int,
-                                                  help="target sequence length")
+                      description="speculative sampling")
+  parser.add_argument(
+           '--target',
+           required=True,
+           help="auto-regressive target model")
+  parser.add_argument(
+           '--draft',
+           required=True,
+           help="auto-regressive draft model")
+  parser.add_argument(
+           '--prompt',
+           required=True,
+           help="initial prompt sequence")
+  parser.add_argument(
+           '--target_sequence_length',
+           required=True,
+           type=int,
+           help="target sequence length")
   
   args = parser.parse_args()
 
